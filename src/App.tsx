87537d390062981b01249b9df2caaf82839cf4b3
eduardoportobaltazar <eduardo.porto.baltazar@gmail.com>
2020-09-02 18:31:18 -0300

    Alua 9

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlobalStyles from './styles/GlobalStyles';
+
+import Dashboard from './pages/DashBoard';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <GlobalStyles />
+           < Dashboard />
+        </>
+    );
+}
+
+export default App;
